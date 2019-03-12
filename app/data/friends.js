fa428e129e94a76754465bb5aa6133f2d0d2493d
Tim Welsh <tim@timwelsh.com>
2019-03-12 16:53:58 -0400

    add base structure

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+// Each of these objects should roughly follow the format below.
+
+// Determine the user's most compatible friend using the following as a guide:
+
+// Convert each user's results into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
+// With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the totalDifference.
+
+// Example:
+// User 1: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+// Use
+// Total Difference: 2 + 1 + 2 = 5
+
+// Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both 5-3 and 3-5 as 2, and so on.
+// The closest match will be the user with the least amount of difference.
+
+// Once you've found the current user's most compatible friend, display the result as a modal pop-up.
+
+// The modal should display both the name and picture of the closest match.
+
+{
+    "name":"Ahmed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
+  
---
+++ server.js
@@ -0,0 +1,4 @@
+const express = require("express");
+require("dotenv").config();
+var keys = require("./keys.js");
+
