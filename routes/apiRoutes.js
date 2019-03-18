108150f2f141f8575560c021c46613a85426e897
Tim Welsh <tim@timwelsh.com>
2019-03-17 22:01:03 -0400

    update home h1

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -16,26 +16,26 @@ module.exports = function(app) {
         var returnArr = [];
         var totalArr = [];
         console.log(" Got here " + req)
-        characterData.forEach(question => {
-            let total = 0;
-            total += Math.abs(parseInt(req.body.q1) - parseInt(question.q1))
-            total += Math.abs(parseInt(req.body.q2) - parseInt(question.q2))
-            total += Math.abs(parseInt(req.body.q3) - parseInt(question.q3))
-            total += Math.abs(parseInt(req.body.q4) - parseInt(question.q4))
-            total += Math.abs(parseInt(req.body.q5) - parseInt(question.q5))
-            total += Math.abs(parseInt(req.body.q6) - parseInt(question.q6))
-            total += Math.abs(parseInt(req.body.q7) - parseInt(question.q7))
-            totalArr.push(total)
-        });
+        // characterData.forEach(question => {
+        //     let total = 0;
+        //     total += Math.abs(parseInt(req.body.q1) - parseInt(question.q1))
+        //     total += Math.abs(parseInt(req.body.q2) - parseInt(question.q2))
+        //     total += Math.abs(parseInt(req.body.q3) - parseInt(question.q3))
+        //     total += Math.abs(parseInt(req.body.q4) - parseInt(question.q4))
+        //     total += Math.abs(parseInt(req.body.q5) - parseInt(question.q5))
+        //     total += Math.abs(parseInt(req.body.q6) - parseInt(question.q6))
+        //     total += Math.abs(parseInt(req.body.q7) - parseInt(question.q7))
+        //     totalArr.push(total)
+        // });
 
-        for (let i = 0; i < totalArr.length; i++) {
-            for (let j = 1; j < totalArr.length; j++) {
-                if(totalArr[i] < totalArr[j])   myMatch = i
-            }
-        }
-        returnArr.push(characterData[myMatch])
+        // for (let i = 0; i < totalArr.length; i++) {
+        //     for (let j = 1; j < totalArr.length; j++) {
+        //         if(totalArr[i] < totalArr[j])   myMatch = i
+        //     }
+        // }
+        // returnArr.push(characterData[myMatch])
 
-        res.json(returnArr);
-        characterData.push(req.body);
+        // res.json(returnArr);
+        // characterData.push(req.body);
     })
 }
