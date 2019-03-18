394f1c0ca16ffc9d05502987f15ea74d7598a55a
Tim Welsh <tim@timwelsh.com>
2019-03-18 15:33:23 -0400

    troubleshooting

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -15,19 +15,19 @@ module.exports = function(app) {
         var myMatch;
         var returnArr = [];
         var totalArr = [];
+        var total = 0;
         console.log(" Got here " + req)
-        // characterData.forEach(question => {
-        //     let total = 0;
-        //     total += Math.abs(parseInt(req.body.q1) - parseInt(question.q1))
-        //     total += Math.abs(parseInt(req.body.q2) - parseInt(question.q2))
-        //     total += Math.abs(parseInt(req.body.q3) - parseInt(question.q3))
-        //     total += Math.abs(parseInt(req.body.q4) - parseInt(question.q4))
-        //     total += Math.abs(parseInt(req.body.q5) - parseInt(question.q5))
-        //     total += Math.abs(parseInt(req.body.q6) - parseInt(question.q6))
-        //     total += Math.abs(parseInt(req.body.q7) - parseInt(question.q7))
-        //     totalArr.push(total)
-        // });
-
+        characterData.forEach(question => {
+            var total = 0;
+            total += Math.abs(parseInt(req.body.q1) - parseInt(question.q1))
+            total += Math.abs(parseInt(req.body.q2) - parseInt(question.q2))
+            total += Math.abs(parseInt(req.body.q3) - parseInt(question.q3))
+            total += Math.abs(parseInt(req.body.q4) - parseInt(question.q4))
+            total += Math.abs(parseInt(req.body.q5) - parseInt(question.q5))
+            total += Math.abs(parseInt(req.body.q6) - parseInt(question.q6))
+            total += Math.abs(parseInt(req.body.q7) - parseInt(question.q7))
+            totalArr.push(total)
+        });
         // for (let i = 0; i < totalArr.length; i++) {
         //     for (let j = 1; j < totalArr.length; j++) {
         //         if(totalArr[i] < totalArr[j])   myMatch = i
