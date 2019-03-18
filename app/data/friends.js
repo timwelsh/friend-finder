949bb443bf78c0e9bdee1145dcefcc8706c923fe
Tim Welsh <tim@timwelsh.com>
2019-03-17 20:06:16 -0400

    progress

--- app/data/friends.js
+++ app/data/friends.js
@@ -16,22 +16,63 @@
 // Once you've found the current user's most compatible friend, display the result as a modal pop-up.
 
 // The modal should display both the name and picture of the closest match.
-const friendData = {
-    name: "Ahmed",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-  };
+const friendData = [
+    {
+        name: "Hamlet",
+        photo: "http://i.huffpost.com/gen/1221096/thumbs/o-KENNETH-BRANAUGH-HAMLET-facebook.jpg",
+        scores: [1, 1, 4, 4, 5, 1, 1],
+        desc: "A tremendously complex person: thoughtful, complex, indecisive, impulsive, careless, melancholy, and more, all at once."
+    },
+    {
+        name: "Coriolanus",
+        photo: "https://www.bucknell.edu/images/Depts/2016News/January/NTLiveCoriolanus800x600.jpg",
+        scores: [4, 5, 4, 3, 3, 2, 5],
+        desc: "An impressive figure who's sometimes too stubborn and proud. You also have a domineering mother. Let's hope you don't end up brutally killed by a group of Roman conspirators."
+    },
+    {
+        name: "Viola from Twelfth Night",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Viola_and_the_Countess_-_Frederick_Richard_Pickersgill.jpg",
+        scores: [2, 1, 3, 2, 4, 4, 4],
+        desc: "A female with an infectious energy, funny, clever, and cunning."
+    },
+    {
+        name: "Sir John Falstaff",
+        photo: "https://stephenbasdeodotorg.files.wordpress.com/2016/01/falstaff.jpg?w=1200",
+        scores: [3, 2, 5, 2, 4, 3, 2],
+        desc: "A bit vain and maybe even a little cowardly, but more importantly the person everyone wants at their party."
+    },
+    {
+        name: "Beatrice from Much Ado About Nothing",
+        photo: "https://mtbifiction.files.wordpress.com/2014/11/0340899_11839_mc_tx3601.jpg?w=665&h=360",
+        scores: [5, 4, 1, 5, 2, 5, 3],
+        desc: "Wise, funny, willing to risk everything, and at times genuinely to be feared. Everyone LOVES you!"
+        },
+    {
+        name: "Othello",
+        photo: "https://i2.wp.com/www.youthvoices.live/wp-content/uploads/2017/04/151110_CBOX_Othello-lawrence-fishburne.jpg.CROP_.promo-xlarge2.jpg?fit=480%2C343&ssl=1",
+        scores: [3, 4, 1, 5, 2, 3, 1],
+        desc: "A tragic life filled with disabling resentment mixed with excessive devotion."
+    },
+    {
+        name: "Lady Macbeth",
+        photo: "http://www.robertmitchellevans.com/wp-content/uploads/2011/01/Lady-Macbeth.jpg",
+        scores: [3, 4, 2, 5, 5, 2, 4],
+        desc: "Enjoys psycho-sexual realtionships, proud manipulator with a demonic heart"
+    },
+    {
+        name: "Titania",
+        photo: "http://landoflegendslv.com/03ports/mystsofavalon/00images/Faeries/Titania_Frederick_Howard_Michael_1896.JPG",
+        scores: [1, 2, 3, 4, 5, 1, 2],
+        desc: "She falls in love with an ass's head"
+    },
+    {
+        name: "King Lear",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/KingLear.jpg/800px-KingLear.jpg",
+        scores: [5, 1, 4, 4, 5, 1, 2],
+        desc: "Divides kingdom in half, is rejected by his daughters, run mad and dies."
+    }
+
+];      
 
   module.exports = friendData;
   
--- server.js
+++ server.js
@@ -4,10 +4,15 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+// for POST request in express
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoute")(app);
 
-
+// listener
 app.listen(PORT, function() {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
